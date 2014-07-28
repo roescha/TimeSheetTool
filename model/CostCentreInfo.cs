5cc8c0ae254b14a6ab4b860c5bbe89eaa16472bb
roescha <alexis.roesch@gmail.com>
2014-07-28 11:33:50 +0100

    latest changes

---
+++ model/CostCentreInfo.cs
@@ -0,0 +1,17 @@
+﻿
+namespace TimeSheetTool.model
+{
+    public class CostCentreInfo
+    {
+        public string Company { get; set; }
+        public string Entity { get; set; }
+        public string CostCentre { get; set; }
+
+        public CostCentreInfo(string company, string entity, string costCentre){
+        this.Company = company;
+        this.Entity = entity;
+        this.CostCentre = costCentre;
+    }
+
+    }
+}
